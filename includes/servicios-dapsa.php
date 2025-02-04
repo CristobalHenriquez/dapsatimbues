381a0d2362bb03cc37aab77c20bcbde21efdde73
Cristobal Henriquez <cristobalhb@live.com>
2025-02-04 17:15:51 -0300

    mmm

---
+++ includes/servicios-dapsa.php
@@ -0,0 +1,75 @@
+<!-- Services Section -->
+<section id="services" class="services section">
+
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
+        <h2>Servicios</h2>
+        <p>Ofrecemos estos dos servicios</p>
+    </div><!-- End Section Title -->
+
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4">
+
+            <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="200">
+                <div class="service-item position-relative">
+                    <div class="img">
+                        <img src="assets/img/services-1.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="details">
+                        <a href="service-details.html" class="stretched-link">
+                            <h3>Cabañas </h3>
+                        </a>
+                        <p>Disfruta de las mejores cabañas</p>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+
+            <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="300">
+                <div class="service-item position-relative">
+                    <div class="img">
+                        <img src="assets/img/services-2.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="details">
+                        <a href="service-details.html" class="stretched-link">
+                            <h3>Hotel</h3>
+                        </a>
+                        <p>Disfruta de las mejores comidas</p>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+<!-- 
+            <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="400">
+                <div class="service-item position-relative">
+                    <div class="img">
+                        <img src="assets/img/services-3.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="details">
+                        <a href="service-details.html" class="stretched-link">
+                            <h3>Ledo Markt</h3>
+                        </a>
+                        <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+                    </div>
+                </div>
+            </div>End Service Item -->
+
+            <!-- <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="500">
+                <div class="service-item position-relative">
+                    <div class="img">
+                        <img src="assets/img/services-4.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="details">
+                        <a href="service-details.html" class="stretched-link">
+                            <h3>Asperiores Commodit</h3>
+                        </a>
+                        <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
+                        <a href="service-details.html" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div>End Service Item -->
+
+        </div>
+
+    </div>
+
+</section><!-- /Services Section -->
