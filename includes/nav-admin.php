0d7bce800f599bbda0b055cb67d60121df97026d
giugerlo <ggiuliano526@gmail.com>
2025-02-22 13:37:59 -0300

    cambios en el nav-admin

--- includes/nav-admin.php
+++ includes/nav-admin.php
@@ -3,20 +3,20 @@
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark nav-admin">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Restaurant</a>
+    <a class="navbar-brand" href="AdministracionProductos">Restaurant</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="admin.php">Productos</a>
+          <a class="nav-link" href="AdministracionProductos">Productos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="admin-categorias.php">Categorías</a>
+          <a class="nav-link" href="AdministracionCategorias">Categorías</a>
         </li>
       </ul>
     </div>
-    <a href="carta.php" class="btn btn-warning">CARTA</a>
+    <a href="Carta" class="btn btn-warning" target="_blank">CARTA</a>
   </div>
 </nav>
